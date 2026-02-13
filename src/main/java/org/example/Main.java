6f70aac88a65e8b5791e67066e630b74cffeae24
Ajayrao29 <ajayrao1294@gmail.com>
2026-02-13 10:48:06 +0530

    Commit Product updations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,93 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         ProductManager manager = new ProductManager();
 
-        manager.addProduct(new Product(1, "iPhone 15", 1200,
-                "Apple", "Mobile", 5, 10));
+        manager.addProduct(new Product(1, "Shoes", 1200,
+                "Nike", "Footwear", 5, 10));
 
-        manager.addProduct(new Product(2, "Galaxy S23", 1000,
-                "Samsung", "Mobile", 4, 15));
+        manager.addProduct(new Product(2, "Jeans", 1000,
+                "Levi's", "Clothing", 4, 15));
 
-        manager.addProduct(new Product(3, "MacBook Air", 1500,
-                "Apple", "Laptop", 5, 20));
+        manager.addProduct(new Product(3, "Vaccum Cleaner", 5000,
+                "Philips", "Appliances", 4, 20));
 
-        manager.addProduct(new Product(4, "Sony Headphones", 300,
-                "Sony", "Accessories", 3, 25));
+        manager.addProduct(new Product(4, "Shirts", 35,
+                "Decathlon", "Sports", 4, 18));
 
-        System.out.println("\n------- Original List ------");
-        manager.displayProducts();
+        manager.addProduct(new Product(5, "Chair", 250,
+                "IKEA", "Furniture", 5, 12));
 
-        System.out.println("\n------ Cost Asc -------");
-        manager.sortByCostAsc();
-        manager.displayProducts();
+        manager.addProduct(new Product(6, "Laptop", 1500,
+                "Apple", "Laptop", 5, 8));
 
-        System.out.println("\n------ Cost Desc ------");
-        manager.sortByCostDesc();
-        manager.displayProducts();
+        manager.addProduct(new Product(7, "TV", 300,
+                "Sony", "Accessories", 4, 25));
 
-        System.out.println("\n------- Discount Asc -------");
-        manager.sortByDiscountAsc();
-        manager.displayProducts();
+        Scanner sc = new Scanner(System.in);
+        int choice;
 
-        System.out.println("\n------- Discount Desc -------");
-        manager.sortByDiscountDesc();
-        manager.displayProducts();
+        do {
+            System.out.println("\n------------- MENU --------------");
+            System.out.println("1. Display Products");
+            System.out.println("2. Sort by Cost Asc");
+            System.out.println("3. Sort by Cost Desc");
+            System.out.println("4. Sort by Discount Asc");
+            System.out.println("5. Sort by Discount Desc");
+            System.out.println("6. Sort by Rating");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
 
-        System.out.println("\n------- Rating --------");
-        manager.sortByRating();
-        manager.displayProducts();
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    manager.displayProducts();
+                    break;
+
+                case 2:
+                    manager.sortByCostAsc();
+                    manager.displayProducts();
+                    break;
+
+                case 3:
+                    manager.sortByCostDesc();
+                    manager.displayProducts();
+                    break;
+
+                case 4:
+                    manager.sortByDiscountAsc();
+                    manager.displayProducts();
+                    break;
+
+                case 5:
+                    manager.sortByDiscountDesc();
+                    manager.displayProducts();
+                    break;
+
+                case 6:
+                    manager.sortByRating();
+                    manager.displayProducts();
+                    break;
+
+                case 0:
+                    System.out.println("Exiting Program...");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice!");
+            }
+
+        } while (choice != 0);
+
+        sc.close();
     }
 }
+
+
